6ec57ef8df11ebdd5df628e5edf22b9cd32b0450
fuad1592 <schifre.15@gmail.com>
2015-11-16 14:26:27 +0700

    Update pertama

--- test-koneksi.php
+++ test-koneksi.php
@@ -3,11 +3,21 @@ error_reporting(0);
 $dbhost = 'localhost';
 $dbuser = 'fuadn';
 $dbpass = 'nasrudin';
+$dbname = 'kemahasiswaan';
+
 $koneksi = mysql_connect($dbhost, $dbuser, $dbpass);
 if (!$koneksi) {
   echo "<script type='text/javascript'>alert('Gagal terhubung dengan database')</script>";
 }
 else {
   echo "<script type='text/javascript'>alert('Berhasil terhubung dengan Database')</script>";
+}
+
+$dbselect = mysql_select_db($dbname);
+if (!$dbselect) {
+  echo "<script type='text/javascript'>alert('Database tidak ada')</script>";
+}
+else {
+  echo "<script type='text/javascript'>alert('Database ada')</script>";
 }
 ?>
