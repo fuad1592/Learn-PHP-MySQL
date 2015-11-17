5ca65d3f553fe4f26a5dea81d4142cb10c007987
fuad1592 <schifre.15@gmail.com>
2015-11-17 22:44:59 +0700

    Second Commit

--- data-mahasiswa.php
+++ data-mahasiswa.php
@@ -7,7 +7,29 @@
   </head>
   <body>
     <?php
-    include 'test-koneksi.php';
+    include "test-koneksi.php";
+    $query = "SELECT * FROM MAHASISWA";
+    $execute = mysql_query($query);
     ?>
+    <table border="1px">
+      <tr>
+        <th>Nomor Mahasiswa</th>
+        <th>Nama Mahasiswa</th>
+        <th>Kode Jurusan</th>
+        <th>Alamat</th>
+      </tr>
+      <tr>
+        <?php
+        while ($row = mysql_fetch_array($execute)) {
+        ?>
+        <td><?php echo "$row[no_mhs]"; ?></td>
+        <td><?php echo "$row[nama]"; ?></td>
+        <td><?php echo "$row[kd_jur]"; ?></td>
+        <td><?php echo "$row[alamat]"; ?></td>
+      </tr>
+      <?php
+      }
+      ?>
+    </table>
   </body>
 </html>
